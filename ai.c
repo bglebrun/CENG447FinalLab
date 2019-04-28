9ed82af8ac85a81758c635cb366e55957b5e22b9
Benjamin LeBrun <bl.lebrun@gmail.com>
2019-04-28 12:43:44 -0600

    Blocking out drive funcitons

--- ai.c
+++ ai.c
@@ -1,1 +1,25 @@
 #include "ai.h"
+
+
+void forwardDrive(int speed, int bias) {
+    // If bias is negative->left, positive->right
+    switch(bias > 0) {
+        case(true):
+        // Right bias
+
+        break;
+        case(false):
+        // Left bias
+
+        break;
+    }
+
+}
+
+void turnRight(int speed) {
+}
+
+// Rip my homie dale earndarht jr
+void turnLeft(int speed) {
+    
+} 
--- ai.h
+++ ai.h
@@ -1,4 +1,13 @@
 #ifndef _AI_H_
 #define _AI_H_
+#include "motor_driver.h"
+#include <stdbool.h>
+#define BIAS_MULT 1
+
+static int bias = 0;
+
+void forwardDrive(int, int);
+void turnRight(int);
+void turnLeft(int);
 
 #endif
