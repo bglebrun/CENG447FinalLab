33e52bc80a53dfca4dee21d3dcc6c273fcd66820
Benjamin Garcia <benjamin.garcia@mines.sdsmt.edu>
2019-04-28 16:06:17 -0600

    begin working on simplified control logic

--- ds4.py
+++ ds4.py
@@ -15,7 +15,8 @@ GATT_CHARACTERISTIC = "0000ffe1-0000-1000-8000-00805f9b34fb"
 mac_addr = "f0:b5:d1:5b:e0:63"  # Ben LeBrun's bot
 # mac_addr = "88:3f:4a:f4:70:e8"  # Ben Garcia's bot
 
-async def write_packet(client: BleakClient, left_forward, right_forward, left_speed, right_speed):
+
+async def write_packet(client: BleakClient, left_forward, left_speed, right_forward, right_speed):
     left_dir = left_forward.to_bytes(1, 'little')
     left_spd = left_speed.to_bytes(1, 'little')
     right_dir = right_forward.to_bytes(1, 'little')
@@ -24,15 +25,16 @@ async def write_packet(client: BleakClient, left_forward, right_forward, left_sp
     await client.write_gatt_char(GATT_CHARACTERISTIC, left_spd)
     await client.write_gatt_char(GATT_CHARACTERISTIC, right_dir)
     await client.write_gatt_char(GATT_CHARACTERISTIC, right_spd)
-    print("wrote packet: %4d | %4d | %4d | %4d" % (left_forward, left_speed, right_forward, right_speed))
+    print("wrote packet: %4d | %4d | %4d | %4d" %
+          (left_forward, left_speed, right_forward, right_speed))
 
 
 class DS4(object):
     controller = None
-    axis_left : float
-    axis_right : float
-    speed_left : int
-    speed_right : int
+    axis_left: float
+    axis_right: float
+    speed_left: int
+    speed_right: int
     dir_left = 1
     dir_right = 1
     quit = False
@@ -54,8 +56,8 @@ class DS4(object):
                 self.quit = True
                 break
             elif event.type == pygame.JOYAXISMOTION:
-                 self.axis_left = round(self.controller.get_axis(1), 3)
-                 self.axis_right = round(self.controller.get_axis(3), 3)
+                self.axis_left = round(self.controller.get_axis(1), 3)
+                self.axis_right = round(self.controller.get_axis(3), 3)
 
         self.speed_left = self.convert_speed(self.axis_left)
         self.speed_right = self.convert_speed(self.axis_right)
@@ -70,7 +72,7 @@ class DS4(object):
         else:
             self.dir_right = 1
 
-    def convert_speed(self, speed : float):
+    def convert_speed(self, speed: float):
         return int((-1) * speed * 255)
 
 
@@ -82,7 +84,7 @@ async def main(mac_addr: str, loop: asyncio.AbstractEventLoop, ds4: DS4):
         print("finished connect")
         while not ds4.quit:
             await ds4.read(pygame.event.get())
-            await write_packet(client, ds4.dir_left, ds4.dir_right, ds4.speed_left, ds4.speed_right)
+            await write_packet(client, ds4.dir_right,  ds4.speed_right, ds4.dir_left,  ds4.speed_left)
             time.sleep(0.1)
 
 
